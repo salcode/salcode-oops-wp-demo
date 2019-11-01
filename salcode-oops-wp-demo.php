780a06d8f5c66f513e768c95ab0afd6d54820f3e
Sal Ferrarello <sal.ferrarello@webdevstudios.com>
2019-11-01 10:15:53 -0400

    Add root plugin file

---
+++ salcode-oops-wp-demo.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: Salcode OOPS-WP Demo
+ * Plugin URI: https://salferrarello.com/
+ * Description: Plugin demonstrating some uses of <a href="https://github.com/WebDevStudios/oops-wp">OOPS-WP</a>.
+ * Version: 1.0.0
+ * Author: Sal Ferrarello
+ * Author URI: http://salferrarello.com/
+ * Text Domain: salcode-oops-wp-demo
+ * Domain Path: /languages
+ *
+ * @package salcode/oops-wp-demo
+ *
+ * @since 1.0.0
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
